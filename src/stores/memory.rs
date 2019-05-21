c0637c4192326cbdd9e663e66aae5319b7264669
David Jonas <djonas@noip.com>
2019-05-21 11:20:13 -0700

    Reduce MemoryStore to single structure to remove logic bug

--- src/stores/memory.rs
+++ src/stores/memory.rs
@@ -12,74 +12,69 @@ use crate::Switch;
 
 #[derive(Debug, Clone)]
 pub struct MemoryStore {
-    ordered: Arc<RwLock<BTreeMap<i64, Vec<String>>>>,
-    switches: Arc<RwLock<HashMap<String, Switch>>>,
+    switches: Arc<RwLock<BTreeMap<i64, HashMap<String, Switch>>>>,
 }
 
 impl MemoryStore {
     pub fn new() -> Self {
         Self {
-            ordered: Arc::new(RwLock::new(BTreeMap::new())),
-            switches: Arc::new(RwLock::new(HashMap::new())),
+            switches: Arc::new(RwLock::new(BTreeMap::new())),
         }
     }
 }
 
 impl Store for MemoryStore {
     fn all(&self) -> Box<Future<Item = Vec<Switch>, Error = ()> + Send> {
-        let all = self
-            .ordered
+        // TODO: don't copy switches
+        let all: Vec<Switch> = self
+            .switches
             .read()
             .iter()
-            .map(|(_, v)| v)
+            .map::<Vec<Switch>, _>(|(_, m)| m.iter().map(|(_, s)| s.to_owned()).collect())
             .flatten()
-            .filter_map(|name| self.switches.read().get(name).map(|s| s.to_owned()))
             .collect();
 
         Box::new(ok(all))
     }
 
     fn expired(&self, when: DateTime<Utc>) -> Box<Future<Item = Vec<Switch>, Error = ()> + Send> {
-        let switches = self.switches.clone();
-
         let expired: Vec<i64> = self
-            .ordered
+            .switches
             .read()
             .range(0..when.timestamp())
-            .map(|(k, _)| *k)
+            .map(|(&k, _)| k)
             .collect();
 
-        let condemned: Vec<Switch> = expired
+        let condemned = expired
             .iter()
-            .filter_map(|k| self.ordered.write().remove(k))
+            .filter_map(|k| self.switches.write().remove(k))
+            .map::<Vec<Switch>, _>(|mut m| m.drain().map(|(_, v)| v).collect())
             .flatten()
-            .filter_map(|name: String| switches.write().remove(&name))
             .collect();
 
-        debug!("condemned: {}", serde_json::to_string(&condemned).unwrap());
         Box::new(ok(condemned))
     }
 
     fn insert(&self, s: Switch) -> Box<Future<Item = (), Error = ()> + Send> {
         debug!("inserting: {:?}", s);
 
-        self.switches.write().insert(s.name.clone(), s.clone());
-
-        debug!("switches: {:?}", self.switches);
-
-        self.ordered
+        self.switches
             .write()
             .entry(s.deadline.timestamp())
             .or_default()
-            .push(s.name.clone());
+            .insert(s.name.clone(), s.clone());
 
-        debug!("ordered: {:?}", self.ordered);
+        debug!("switches: {:?}", self.switches);
 
         Box::new(futures::future::ok(()))
     }
 
     fn take(&self, name: &str) -> Box<Future<Item = Option<Switch>, Error = ()> + Send> {
-        let s = self.switches.write().remove(name);
+        let s = self
+            .switches
+            .write()
+            .iter_mut()
+            .find_map(|(_, m)| m.remove(name));
         Box::new(ok(s))
     }
 }
